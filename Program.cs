510c34bd2e4328097fc183e8041ca0f23504bbd8
shaunawins <shnwntrs@gmail.com>
2021-01-28 20:06:32 -0500

    Actually added the Program.cs this time. Note to self: Git Add is your friend

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using System.Xml.Linq;
+using System;
+using NfoSharp.NfoDtos;
+
+namespace NfoSharp
+{
+    public class NfoSharp
+    {
+        public static void Main(string[] args)
+        {
+            var m = new Movie();
+            m.Title = "Movie Title";
+
+        }
+    }
+}
